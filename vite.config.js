d4ffd5acf9e6e750a37f3c0961ce533571ca49ed
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-08-14 22:15:54 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: './src/main.tsx'
     }
-  }
+  },
+  plugins: [ react() ],
+  base: '/weather-dashboard/',
 })
