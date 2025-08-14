de4cec1fe0a91fc44ca4cda8d51a4d031168289e
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-08-14 16:30:43 +0300

    add

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
+import Home from "./pages/Home";
+import Forecast from "./pages/Forecast";
